01df38ea2bc6417f23e4eab3e55c94cf6d5c4cf5
nghiemkythu <nghiemdt2013@gmail.com>
2022-12-10 23:03:40 +0700

    add assigment operator tp Point.h

--- fileh/Point.h
+++ fileh/Point.h
@@ -9,7 +9,12 @@ public:
 	Point() { pos = sf::Vector2f(0.f, 0.f); };
 	Point(double x, double y) { pos = sf::Vector2f(x, y); };
 	Point(const Point& src): pos(src.pos) {};
-
+	Point& operator=(const Point& src){
+		if(this == &src) return *this;
+		pos.x = src.pos.x;
+		pos.y = src.pos.y;
+		return *this;
+	}
 	void updatePoint(double x, double y) {
 		this->pos.x += x;
 		this->pos.y += y; 
