5695c447716226696cb04a64ad5b0a27f28b8960
ttviet2805 <ttviet2805@gmail.com>
2022-12-11 23:38:50 +0700

    Update rectangle class

--- filecpp/CRoad.cpp
+++ filecpp/CRoad.cpp
@@ -2,13 +2,50 @@
 #include "../fileh/Window.h"
 #include "../fileh/Player.h"
 
+#include "../fileh/Rectangle.h"
+
+using namespace sf;
+
 int main()
 {
-    Player* player = new Player(sf::Vector2u(10, 2), 0.1f);
+    //Player* player = new Player(sf::Vector2u(10, 2), 0.1f);
 
-    Window game(1300, 800);
-    game.run(player);
+    //Window game(1300, 800);
+    //game.run(player);
 
-    delete player;
+    //delete player;
+
+    RenderWindow window(VideoMode(1080, 720), "My Window");
+
+    Texture character;
+    character.loadFromFile("image/Amongus.png");
+    Rectangle tmpRect(Vector2f(150, 200), Vector2f(200, 200), character);
+
+    Texture newCharacter;
+    newCharacter.loadFromFile("image/Songoku.png");
+    Rectangle newRect(Vector2f(150, 200), Vector2f(600, 600), newCharacter);
+
+
+    while (window.isOpen()) {
+        Event event;
+
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear(Color::White);
+
+        window.draw(tmpRect.getRect());
+        window.draw(newRect.getRect());
+
+        tmpRect.characterMove(1);
+
+        tmpRect.isCollision(newRect);
+
+
+        window.display();
+    }
+
     return 0;
 }
---
+++ fileh/Rectangle.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
+
+class Rectangle {
+private:
+	RectangleShape Rect;
+
+public:
+	// Rectangle has Size, Position and Image
+	// Texture is image
+	Rectangle(Vector2f _size, Vector2f _position, Texture& _texture) {
+		Rect.setSize(_size);
+		Rect.setPosition(_position);
+		Rect.setTexture(&_texture);
+	}
+
+	~Rectangle() {}
+
+	// get Rectangle
+	RectangleShape getRect() {
+		return Rect;
+	}
+		
+	// set image for Rectangle
+	void setTexture(Texture& _texture) {
+		Rect.setTexture(&_texture);
+	}
+
+	Vector2f getPosition() {
+		return Rect.getPosition();
+	}
+	
+	// add x-axis position X
+	// add y-axis position Y
+	void move(float X, float Y) {
+		Rect.move(X, Y);
+	}
+	
+	// use this function to check move up, move down, move left, move right
+	// this function don't return any thing
+	void characterMove(float dentaTime) {
+		if (Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			Rect.move(-dentaTime, 0);
+		}
+		else {
+			if (Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+				Rect.move(dentaTime, 0);
+			}
+			else {
+				if (Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+					Rect.move(0, -dentaTime);
+				}
+				else {
+					if (Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+						Rect.move(0, dentaTime);
+					}
+				}
+			}
+		}
+	}
+
+	// to check if mouse click or not
+	// 0: not click
+	// 1: left mouse click
+	// 2: right mouse click
+	int is_Clicked(Vector2f Position) {
+		int isPress = 0;
+
+		if (Rect.getGlobalBounds().contains(Position)) {
+			while (Mouse::isButtonPressed(Mouse::Left)) isPress = 1;
+
+			if (isPress == 1) return 1;
+
+			return 2;
+		}
+
+		return 0;
+	}
+
+	// to check if 2 rectangle is collision or not
+	bool isCollision(const Rectangle& X) {
+		const bool collides = Rect.getGlobalBounds().intersects(X.Rect.getGlobalBounds());
+		
+		if (collides) cout << "Collision\n";
+		return collides;
+	}
+};
