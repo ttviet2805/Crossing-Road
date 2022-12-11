c90a524491a18d40c625d73bb4d897e369abe573
trnthienphc2003 <88192288+trnthienphc2003@users.noreply.github.com>
2022-12-11 20:51:59 +0700

    Implement basic methods for class Player
    Basic methods for class Player (will need extra modifications later)

---
+++ filecpp/Player.cpp
@@ -0,0 +1,30 @@
+#include "../fileh/Player.h"
+
+void Player::changeSpeed(float speed) {
+    curSpeed=speed;
+}
+
+void Player::move(int direction) {
+    switch(direction) {
+        case 1: // move up
+            curPos.moveUp(curSpeed);
+            break;
+        case 2: // move down
+            curPos.moveDown(curSpeed);
+            break;
+        case 3: // move left
+            curPos.moveLeft(curSpeed);
+            break;
+        case 4: // move right
+            curPos.moveRight(curSpeed);
+            break;
+        default:
+            break;
+    }
+
+    observer->notify(curPos);
+}
+
+void Player::updateInfo(std::string newName) {
+    name=newName;
+}
